5379d6551b4def167d07761d85186295d4ad90a4
Jose Valenzuela <josev34@Desktop-3417.Home-net3417>
2018-02-22 16:00:46 -0800

    update

--- lab3.py
+++ lab3.py
@@ -2,17 +2,68 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import random 
 
-data[][2] = np.loadtxt('Lab3.txt')
-
+data = np.loadtxt('Lab3.txt')
 np.random.shuffle(data)
-cent1 = data[0]
-cent2 = data[1]
-print data
 
-np.expand_dims(data[:,2], axis = 2)
+plt.scatter(x = data[:,0], y = data[:,1], s = 2)
+plt.show()
 
-data[0][3].append("color") 
-print data
-print cent1
-print cent2
-#plt.scatter(x = data[:,0], y = data[:,1], s = 2)
+colors = 10*["g","r","c","b","k"]
+
+class K_Means:
+	def __init__(self, k=2, tol=0.001, max_iter=300):
+		self.k = k
+		self.tol = tol
+		self.max_iter = max_iter
+
+	def fit(self, data):
+		
+		self.centroids = {}
+
+		for i in range(self.k):
+			self.centroids[i] = data[i]
+
+		for i in range(self.max_iter):
+			self.classifications = {}
+
+			for i in range(self.k):
+				self.classifications[i] = []
+
+			for featureset in data:
+				distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+				classification = distances.index(min(distances))
+				self.classifications[classification].append(featureset)
+
+			prev_centroids = dict(self.centroids)
+
+			for classification in self.classifications:
+				self.centroids[classification] = np.average(self.classifications[classification], axis=0)
+	
+			optimized = True
+
+			for c in self.centroids:
+				original_centroid = prev_centroids[c]
+				current_centroid = self.centroids[c]
+				if np.sum((current_centroid-original_centroid)/original_centroid*100.00) > self.tol:
+					optimized = False
+
+			if optimized:
+				break
+
+	def predict(self, data):
+		distances = [np.linalg.norm(data-self.centroids[centroids]) for centroid in self.centroids]
+		classification = distances.index(min(distances))
+		return classification 
+
+clf = K_Means()
+clf.fit(data)
+
+for centroid in clf.centroids:
+	plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1],
+		marker="o", color="k", s=15, linewidths=5)
+for classification in clf.classifications:
+	color = colors[classification]
+	for featureset in clf.classifications[classification]:
+		plt.scatter(featureset[0], featureset[1], marker="x", color=color, s=5, linewidths=3)
+
+plt.show()
